66e4e534db24f14e82c8ea23ed7a009ac625b5cd
Joao Moreno <joao.moreno@microsoft.com>
2021-12-16 20:26:33 +0100

    update name

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,15 +1,14 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "jsnb" is now active in the web extension host!'
+  let disposable = vscode.commands.registerCommand(
+    "javascript-notebooks.helloWorld",
+    () => {
+      vscode.window.showInformationMessage(
+        "Hello World from JavaScript Notebooks in a web extension host!"
+      );
+    }
   );
-
-  let disposable = vscode.commands.registerCommand("jsnb.helloWorld", () => {
-    vscode.window.showInformationMessage(
-      "Hello World from JavaScript Notebooks in a web extension host!"
-    );
-  });
 
   context.subscriptions.push(disposable);
 }
