edc9a9601183c3e4777f1dd8778ca33f074be536
Joao Moreno <joao.moreno@microsoft.com>
2021-12-17 15:19:15 +0100

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 const path = require('path');
-const webpack = require('webpack');
 
 /** @type WebpackConfig[] */
 module.exports = [{
@@ -18,8 +17,7 @@ module.exports = [{
 	target: 'webworker',
 	entry: {
 		'extension': './src/extension.ts',
-		'worker': './src/worker.ts',
-		'test/suite/index': './src/test/suite/index.ts'
+		'worker': './src/worker.ts'
 	},
 	output: {
 		filename: '[name].js',
@@ -29,15 +27,6 @@ module.exports = [{
 	resolve: {
 		mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
 		extensions: ['.ts', '.js'], // support ts-files and js-files
-		alias: {
-			// provides alternate implementation for node module and source files
-		},
-		fallback: {
-			// Webpack 5 no longer polyfills Node.js core modules automatically.
-			// see https://webpack.js.org/configuration/resolve/#resolvefallback
-			// for the list of Node.js core module polyfills.
-			'assert': require.resolve('assert')
-		}
 	},
 	module: {
 		rules: [{
@@ -48,11 +37,6 @@ module.exports = [{
 			}]
 		}]
 	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			process: 'process/browser', // provide a shim for the global `process` variable
-		}),
-	],
 	externals: {
 		'vscode': 'commonjs vscode', // ignored because it doesn't exist
 	},
